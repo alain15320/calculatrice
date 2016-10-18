06f095373f705ad90247666b156ccf5fb2cbad0a
alain <alain15320@gmail.com>
2016-10-18 19:46:46 +0200

    Integration de la première classe de test AdditionTest Debut de restructuration des répertoires

---
+++ tests/math/AdditionTest.java
@@ -0,0 +1,32 @@
+package math;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AdditionTest {
+
+	protected Addition op;
+	
+	@Before
+	public void setUp() throws Exception {
+		op = new Addition();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCalculer() throws Exception {
+		assertEquals(new Long(4), op.calculer(new Long(1), new Long(3)));
+	}
+	
+	@Test
+	public void testLireSymbole() throws Exception {
+		assertEquals((Character)'+', op.lireSymbole());
+	}
+
+}
